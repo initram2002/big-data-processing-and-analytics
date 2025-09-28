64022d3881045471ea1bc85cb7dab7439f3d2380
Michela Martini <martini.michela02@gmail.com>
2025-09-28 10:40:46 +0200

    Refactor Person class to improve encapsulation and add getters/setters

--- Additional material/MyFirstProject/src/MyApp.java
+++ Additional material/MyFirstProject/src/MyApp.java
@@ -9,15 +9,29 @@ public class MyApp {
 
         p . print();
 
-        // p.firstName = "NOTHING"; // NOT VISIBLE!!!
+        // p.firstName = "NOTHING"; // NOT VISIBLE - private field
 
         Person q = new Person("Mario", "Rossi", "1");
 
         q.print();
 
+        // q.ssn = "12345"; // NOT VISIBLE - now private for proper encapsulation
 
-        Person r = new Person();
+        // Use setter instead
 
+        q.setSsn("12345");
+
+        System.out.println("Updated SSN: " + q.getSsn());
+
+        Person r = new Person(); // uses default constructor
+
+        r.print();
+
+        // Demonstrate getters and setters
+        r.setFirstName("Alice");
+        r.setLastName("Johnson");
+        System.out.println("\nAfter modification:");
+        r.print();
 
     }
 
--- Additional material/MyFirstProject/src/Person.java
+++ Additional material/MyFirstProject/src/Person.java
@@ -1,25 +1,51 @@
 
 public class Person {
-    // Attributes
+    // Attributes - make ssn private for proper encapsulation
     private String firstName = "John";
     private String lastName = "Smith";
     private String ssn = "0";
 
-    public Person(String f, String l, String s) {
-        firstName = f;
-        lastName = l;
-        ssn = s;
+    public Person(String firstName, String lastName, String ssn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
     }
 
     public Person() {
-        // ...
-        ssn = "0";
+        this.firstName = "John";
+        this.lastName = "Smith";
+        this.ssn = "0";
     }
 
     public void print() {
-        System.out.println(firstName);
-        System.out.println(lastName);
-        System.out.println(ssn);
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("SSN: " + ssn);
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
     }
 
 }
